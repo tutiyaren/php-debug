e8ad3848eee183a45422ed5a21b4664eee219947
gotokouichi <koichi.110913@icloud.com>
2022-11-08 22:41:03 +0900

    if文の条件修正

--- src/public/user/signin_complete.php
+++ src/public/user/signin_complete.php
@@ -8,28 +8,31 @@ if (empty($email) || empty($password)) {
 }
 
 if (empty($errors)) {
-    $dbUserName = 'root';
-    $dbPassword = 'password';
-    $pdo = new PDO(
-        'mysql:host=mysql; dbname=memo; charset=utf8',
-        $dbUserName,
-        $dbPassword
-    );
+    if (password_verify($password, $singin_password)) {
+        $dbUserName = 'root';
+        $dbPassword = 'password';
+        $pdo = new PDO(
+            'mysql:host=mysql; dbname=memo; charset=utf8',
+            $dbUserName,
+            $dbPassword
+        );
 
-    $stmt = " SELECT
+        $stmt = " SELECT
     *
     FROM users where email = '$email' ";
-    $stmt = $pdo->prepare($stmt);
-    $stmt->bindValue('email', $email, PDO::PARAM_INT);
-    $stmt->execute();
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $pdo->prepare($stmt);
+        $stmt->bindValue('email', $email, PDO::PARAM_INT);
+        $stmt->execute();
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($users as $user) {
-        $singin_password = $user['password'];
+        foreach ($users as $user) {
+            $singin_password = $user['password'];
+        }
+    } else {
+        $errors[] = 'メールアドレスまたはパスワードが違います';
     }
 }
 ?>
-
 
 <!DOCTYPE html>
 <html>
